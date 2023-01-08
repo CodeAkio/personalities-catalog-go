b44c3ee0fd7f070444341a68a754afc8ed28e314
CodeAkio <siqueira_2004@hotmail.com>
2023-01-08 11:26:41 -0300

    fix: remove in memory personalities

--- main.go
+++ main.go
@@ -4,16 +4,10 @@ import (
 	"fmt"
 
 	"github.com/CodeAkio/personalities-catalog-go/database"
-	"github.com/CodeAkio/personalities-catalog-go/models"
 	"github.com/CodeAkio/personalities-catalog-go/routes"
 )
 
 func main() {
-	models.Personalities = []models.Personality{
-		{Id: 1, Name: "Name 1", History: "History 1"},
-		{Id: 2, Name: "Name 2", History: "History 2"},
-	}
-
 	database.ConnectDb()
 
 	fmt.Println("🚀 Starting server at: http://localhost:8000")
